0ce72da70c6c32f30cd739292d4f4a9a726db13e
Sergii Zhevzhyk <vzhikserg@gmail.com>
2018-09-22 22:13:09 +0200

    Changing to the euro sign

--- components/PriceMarker.js
+++ components/PriceMarker.js
@@ -22,7 +22,7 @@ class PriceMarker extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.bubble}>
-          <Text style={styles.dollar}>$</Text>
+          <Text style={[styles.dollar, { fontSize }]}>€</Text>
           <Text style={[styles.amount, { fontSize }]}>{amount}</Text>
         </View>
         <View style={styles.arrowBorder} />
